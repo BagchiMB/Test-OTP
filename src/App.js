6726432e1afb78f5c59c4ed2b9c6f346c5897664
BagchiMB <mbagchi6884@gmail.com>
2020-09-12 00:01:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import OtpInput from 'react-otp-input';
+
+const OTPSET=['1468','4793','1234','0000'];
 
 function App() {
+
+  const [validator,setValidator]=React.useState(false);
+  const [otp,setOtp]=React.useState('')
+  const [result,setResult]=React.useState('')
+
+  const handleGetOTPSubmit = () => 
+  {
+    setResult(OTPSET[Math.floor(Math.random()*4)])
+    setValidator(true)
+  }
+
+  const handleChange = (otp)=>
+  {
+    setOtp(otp)
+  }
+
+  const handleSubmit=()=>
+  {
+    if(otp===result)
+      alert("success")
+    else
+      alert("Failed")
+
+    setResult('')
+    setValidator(false)
+    setOtp('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="Abs left">OTP VALIDATOR</p>
+      <p className="Abs right">OTP VALIDATOR</p>
+      <div className="content">
+        {!validator?<button onClick={handleGetOTPSubmit}>Generate OTP</button>:
+          <div className="Inp">
+              <OtpInput
+              value={otp}
+              onChange={handleChange}
+              numInputs={4}
+              separator={<span>&nbsp;&nbsp;&nbsp;</span>}
+              inputStyle={{width:'5rem',height:'5rem',outline:'none',fontSize:'2rem'}}
+              shouldAutoFocus={true}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+          </div>}
+      </div>
     </div>
   );
 }
